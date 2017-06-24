8e00a541e31efee334bd565aa6a789378f720c1b
Scott Windsor <swindsor@gmail.com>
2017-06-24 10:16:03 -0700

    Adding some tap and holds

--- keyboards/atreus/keymaps/sentientmonkey/keymap.c
+++ keyboards/atreus/keymaps/sentientmonkey/keymap.c
@@ -15,8 +15,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = { /* Qwerty */
   {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_TRNS,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
   {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_TRNS,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
-  {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LCTL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
-  {KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT  }
+  {SFT_T(KC_Z),KC_X, KC_C,    KC_V,    KC_B,   ALT_T(KC_ENT),  KC_N,    KC_M,    KC_COMM, KC_DOT, SFT_T(KC_SLSH)},
+  {KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC, CTL_T(KC_SPC), KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT  }
 },
 /*
  *  !       @     up     {    }        ||     pgup    7     8     9    *

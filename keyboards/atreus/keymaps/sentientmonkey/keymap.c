0f185b15ce18becbebadf5bb2a44d73bed6a440e
Scott Windsor <swindsor@gmail.com>
2018-01-09 19:13:05 -0800

    Update ergodox laytout to be more atreus-y

--- keyboards/atreus/keymaps/sentientmonkey/keymap.c
+++ keyboards/atreus/keymaps/sentientmonkey/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_TRNS,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
   {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_TRNS,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
   {SFT_T(KC_Z),KC_X, KC_C,    KC_V,    KC_B,   ALT_T(KC_ENT),  KC_N,    KC_M,    KC_COMM, KC_DOT, SFT_T(KC_SLSH)},
-  {KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC, CTL_T(KC_SPC), KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT  }
+  {LT(_LW,KC_ESC), KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC, CTL_T(KC_SPC), KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT  }
 },
 /*
  *  !       @     up     {    }        ||     pgup    7     8     9    *
@@ -33,13 +33,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
  *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
- *       volup             reset      ||     sft     F1    F2    F3   F12
+ *  cad  volup             reset      ||     sft     F1    F2    F3   F12
  *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
  */
 [_LW] = { /* [> LOWER <] */
   {KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10},
   {KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_TRNS, KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11},
-  {KC_NO, KC_VOLU, KC_NO,   KC_NO,   RESET,   KC_LCTL, LALT(LCTL(KC_LGUI)),   KC_F1,   KC_F2,   KC_F3,   KC_F12},
+  {LCTL(LALT(KC_DELT)), KC_VOLU, KC_NO,   KC_NO,   RESET,   KC_LCTL, LALT(LCTL(KC_LGUI)),   KC_F1,   KC_F2,   KC_F3,   KC_F12},
   {KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS}
 }};
 
--- layouts/community/ergodox/sentientmonkey/keymap.c
+++ layouts/community/ergodox/sentientmonkey/keymap.c
@@ -18,43 +18,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | ESC  |           | TT2  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  | ESC  |           | TT2  |   6  |   7  |   8  |   9  |   0  |  Del   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  TT1 |           | TT1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShft  |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShft  |
+ * | LShft  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShft  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |  Tab |[/LGui|   ]  | LGui |                                       |  L1  |   -  |   '  |Enter |      |
+ *   |  `   |  Esc | Tab  | LGui | Shft |                                       |  L1  |   -  |   '  | Enter|      |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | Alt  | LCtl |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Backs| LCtl |------|       |------| LAlt   |Space |
- *                                 | pace |      | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                        ,-------------.       ,--------------.
+ *                                        | Alt  | LCtl |       | Alt  | Rctrl |
+ *                                 ,------|------|------|       |------+-------+------.
+ *                                 |      |      | Home |       | PgUp |       |      |
+ *                                 | Backs| LCtl |------|       |------| LAlt  |Space |
+ *                                 | pace |      | End  |       | PgDn |       |      |
+ *                                 `--------------------'       `---------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_ESC,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_TRNS,        KC_ESC,       KC_TAB,KC_LGUI,KC_LSFT,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_GRV,        KC_ESC,       KC_TAB,KC_LGUI,KC_LSFT,
                                                     KC_LALT,  KC_LCTL,
                                                               KC_HOME,
                                                 KC_BSPC,KC_LCTL,KC_END,
         // right hand
-             TT(2),       KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TT(1),       KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             TT(2),       KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_DELT,
+             KC_RBRC,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
+                          KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
+             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                               MO(SYMB),  KC_MINS,KC_QUOT,KC_ENT,           KC_TRNS,
-             KC_LALT,        CTL_T(KC_ESC),
+             KC_RALT,        KC_RCTL,
              KC_PGUP,
              KC_PGDN,KC_LALT, KC_SPC
     ),
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift  |   [  |   ]  |   (  |   )  |   &  |      |           |      |   `  |   1  |   2  |   3  |   \  | RShft  |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |       |  L2  | LGui | Shift| Bksp |                                       |      |   .  |   0  |   =  |      |
+ *   |       |  L2  |  Tab | LGui | Shft |                                       |      |   .  |   0  |   =  |      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | LAlt |      |       | RAlt |      |

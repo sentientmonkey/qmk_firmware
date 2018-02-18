fce45b4341f5a5dea724ced78029a9addede0f0d
Scott Windsor <swindsor@gmail.com>
2018-02-18 10:50:48 -0800

    Add tap/hold for a/L2 on atreus

--- keyboards/atreus/keymaps/sentientmonkey/keymap.c
+++ keyboards/atreus/keymaps/sentientmonkey/keymap.c
@@ -14,7 +14,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = { /* Qwerty */
   {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_TRNS,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
-  {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_TRNS,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
+  {LT(_LW,KC_A),KC_S,    KC_D,    KC_F,    KC_G,   KC_TRNS,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
   {SFT_T(KC_Z),KC_X, KC_C,    KC_V,    KC_B,   ALT_T(KC_ENT),  KC_N,    KC_M,    KC_COMM, KC_DOT, SFT_T(KC_SLSH)},
   {LT(_LW,KC_ESC), KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC, CTL_T(KC_SPC), KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT  }
 },
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LW] = { /* [> LOWER <] */
   {KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10},
-  {KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_TRNS, KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11},
+  {LT(_LW,KC_A), KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_TRNS, KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11},
   {LCTL(LALT(KC_DELT)), KC_VOLU, KC_NO,   KC_NO,   RESET,   KC_LCTL, LALT(LCTL(KC_LGUI)),   KC_F1,   KC_F2,   KC_F3,   KC_F12},
   {KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS}
 }};

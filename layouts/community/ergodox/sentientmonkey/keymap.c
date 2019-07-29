d12a33adef1bb4d92797df9a1974870129415388
Scott Windsor <swindsor@gmail.com>
2019-07-28 17:51:48 -0700

    tweaks

--- layouts/community/ergodox/sentientmonkey/keymap.c
+++ layouts/community/ergodox/sentientmonkey/keymap.c
@@ -16,7 +16,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
- *
+                                       *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  | ESC  |           | TT2  |   6  |   7  |   8  |   9  |   0  |  Del   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShft  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShft  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  `   |  Esc | Tab  | LGui | Shft |                                       |  L1  |   -  |   '  | Enter|      |
+ *   |  `   |  Esc | Tab  | LGui | Shft |                                       |  L1  |   -  |   '  | Enter| CAG  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
  *                                        | Alt  | LCtl |       | Alt  | Rctrl |
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_ESC,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
         KC_LCTL,       LT(MDIA,KC_A), KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   LCAG(KC_NO),
         KC_GRV,        KC_ESC,       KC_TAB,KC_LGUI,KC_LSFT,
                                                     KC_LALT,  KC_LCTL,
                                                               KC_HOME,
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TT(2),       KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_DELT,
              KC_RBRC,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                              MO(SYMB),  KC_MINS,KC_QUOT,KC_ENT,           KC_TRNS,
+             LCAG(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+                              MO(SYMB),  KC_MINS,KC_QUOT,KC_ENT,           RCTL(RALT(KC_RGUI)),
              KC_RALT,        KC_RCTL,
              KC_PGUP,
              KC_PGDN,KC_LALT, KC_SPC
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift  |   [  |   ]  |   (  |   )  |   &  |      |           |      |   `  |   1  |   2  |   3  |   \  | RShft  |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |       |  L2  |  Tab | LGui | Shft |                                       |      |   .  |   0  |   =  |      |
+ *   |       |  L2  |  Tab | LGui | Shft |                                       |      |   .  |   0  |   =  | CAG  |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |  Alt |      |       | RAlt |      |
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_PGUP, KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_PGDN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_GRV,  KC_1,   KC_2,    KC_3,    KC_BSLS, KC_RSFT,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  RCTL(RALT(KC_RGUI)),
        KC_RALT, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_RALT, KC_SPC
